9e2a6d57687a318ec6cee90b30c7685e9b7619da
ct420h <ct420h@att.com>
2024-04-26 11:32:21 -0700

    quick save wip

--- cdk-starter/lib/cdk-starter-stack.ts
+++ cdk-starter/lib/cdk-starter-stack.ts
@@ -29,8 +29,17 @@ export class CdkStarterStack extends cdk.Stack {
       },
     });
 
+    const duration = new cdk.CfnParameter(this, 'duration', {
+      default: 6, 
+      minValue: 1,
+      maxValue: 10,
+      type: 'Number'
+    })
+
     const myL2Bucket = new Bucket(this, "MyL2Bucket", {
-      lifecycleRules: [{ expiration: cdk.Duration.days(2) }],
+      lifecycleRules: [{ 
+        expiration: cdk.Duration.days(duration.valueAsNumber) 
+      }],
     });
     
     new cdk.CfnOutput(this, "MyL2BucketName", { value: myL2Bucket.bucketName });
