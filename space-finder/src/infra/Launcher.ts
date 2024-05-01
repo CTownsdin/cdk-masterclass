578a660af2b9366a9ae40794f89879c13dcf1858
ct420h <ct420h@att.com>
2024-05-01 16:25:50 -0700

    quick save wip

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -1,9 +1,11 @@
 import { App } from "aws-cdk-lib"
 import { DataStack } from "./stacks/DataStack"
+import { LambdaStack } from "./stacks/LambdaStack";
 
 
 
 const app = new App();
 
-new DataStack(app, 'Datastack')
+new DataStack(app, 'DataStack')
+new LambdaStack(app, 'LambdaStack')
 
