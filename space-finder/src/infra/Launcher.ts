5f0f269493bfb0947f9d796dd5a22e107cebdc94
ct420h <ct420h@att.com>
2024-05-07 16:19:40 -0700

    add api gw stack

--- space-finder/src/infra/Launcher.ts
+++ space-finder/src/infra/Launcher.ts
@@ -1,11 +1,15 @@
-import { App } from "aws-cdk-lib"
-import { DataStack } from "./stacks/DataStack"
+import { App } from "aws-cdk-lib";
+import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
-
+import { ApiStack } from "./stacks/ApiStack";
 
 
 const app = new App();
 
-new DataStack(app, 'DataStack')
-new LambdaStack(app, 'LambdaStack')
+const dataStack = new DataStack(app, "DataStack");
 
+const lambdaStack = new LambdaStack(app, "LambdaStack");
+
+const apiStack = new ApiStack(app, "ApiStack", {
+  helloLambdaIntegration: lambdaStack.helloLambdaIntegration,
+});
