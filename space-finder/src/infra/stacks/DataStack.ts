2177c192e6ecc634cfd0d53703e92cd455aefeb9
ct420h <ct420h@att.com>
2024-05-09 15:54:53 -0700

    save suffix fn

--- space-finder/src/infra/stacks/DataStack.ts
+++ space-finder/src/infra/stacks/DataStack.ts
@@ -1,9 +1,20 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 
 export class DataStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps){
     super(scope, id, props)
+
+    // DDB table
+    new Table(this, 'SpacesTable', { 
+      partitionKey: { 
+        name: 'id', 
+        type: AttributeType.STRING 
+      },
+      tableName: 'SpacesStack',
+      
+    })
   }
 }
